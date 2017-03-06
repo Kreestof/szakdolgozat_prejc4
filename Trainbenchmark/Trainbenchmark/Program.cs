ff56bb31f1a504391ffc49ce7694f5ef59872dfe
Kreestof <kikko634@gmail.com>
2017-03-06 19:31:38 +0100

    Beolvasás 1.0
    A beolvasás elvileg működik, még minden ömlesztve a Main-be statikus
    függvényekkel. Trackelement kérdéses még.

--- Trainbenchmark/Trainbenchmark/Program.cs
+++ Trainbenchmark/Trainbenchmark/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Trinity;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+using TrainBenchmarkTSLProject;
 
 namespace Trainbenchmark
 {
@@ -11,7 +14,200 @@ namespace Trainbenchmark
     {
         static void Main(string[] args)
         {
-            // Add a TSL project to leverage data modeling capabilities.
+            TrinityConfig.CurrentRunningMode = RunningMode.Embedded;
+            IGraph g = new Graph();
+            TurtleParser ttlparser = new TurtleParser();
+            //El kell helyezni az állományt!
+            ttlparser.Load(g, @"c:\Kristof\BME\szakdolgozat\Trainbenchmark\Trainbenchmark\sources\railway-batch-1-inferred.ttl");
+
+            var typeNode = g.CreateUriNode("rdf:type");
+            var triples = g.GetTriplesWithPredicate(typeNode);
+            foreach (var b in triples)
+            {
+                switch (((UriNode)b.Object).Uri.Fragment)
+                {
+                    case "#Semaphore":
+                        int semaphoreId = int.Parse(((UriNode)b.Subject).Uri.Fragment.Substring(2));
+                        Signal semaphoreSignal = Signal.FAILURE;
+                        foreach (var triple in g.GetTriplesWithSubject(b.Subject))
+                        {
+                            if (triple.Predicate.ToString().Split('#')[1] == "signal")
+                            {
+                                semaphoreSignal = getSignalFromString(((UriNode)triple.Object).Uri.Fragment.Substring(1));
+                            }
+                        }
+                        Semaphore semaphore = new Semaphore(cell_id: semaphoreId, signal: semaphoreSignal);
+                        Global.LocalStorage.SaveSemaphore(semaphore);
+                        break;
+                    case "#Route":
+                        int routeId = int.Parse(((UriNode)b.Subject).Uri.Fragment.Split('_')[1]);
+                        bool routeActive = false;
+                        int routeEntry = -1;
+                        int routeExit = -1;
+                        List<int> routeRequires = new List<int>();
+                        List<int> routeFollows = new List<int>();
+                        foreach (var triple in g.GetTriplesWithSubject(b.Subject))
+                        {
+                            switch (triple.Predicate.ToString().Split('#')[1])
+                            {
+                                case "active":
+                                    routeActive = ((LiteralNode)triple.Object).Value == "true";
+                                    break;
+                                case "entry":
+                                    routeEntry = int.Parse(((UriNode)triple.Object).Uri.Fragment.Substring(2));
+                                    break;
+                                case "exit":
+                                    routeExit = int.Parse(((UriNode)triple.Object).Uri.Fragment.Substring(2));
+                                    break;
+                                case "requires":
+                                    routeRequires.Add(int.Parse(((UriNode)triple.Object).Uri.Fragment.Substring(2)));
+                                    break;
+                                case "follows":
+                                    routeFollows.Add(int.Parse(((UriNode)triple.Object).Uri.Fragment.Substring(2)));
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        Route route = new Route(cell_id: routeId, requires: routeRequires, follows: routeFollows, entry: routeEntry, exit: routeExit, active: routeActive);
+                        Global.LocalStorage.SaveRoute(route);
+                        break;
+                    case "#Region":
+                        int regionId = int.Parse(((UriNode)b.Subject).Uri.Fragment.Substring(2));
+                        List<int> regionSensors = new List<int>();
+                        List<int> regionElements = new List<int>();
+                        foreach (var triple in g.GetTriplesWithSubject(b.Subject))
+                        {
+                            switch (triple.Predicate.ToString().Split('#')[1])
+                            {
+                                case "sensors":
+                                    regionSensors.Add(int.Parse(((UriNode)triple.Object).Uri.Fragment.Substring(2)));
+                                    break;
+                                case "elements":
+                                    regionElements.Add(int.Parse(((UriNode)triple.Object).Uri.Fragment.Substring(2)));
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        Region region = new Region(cell_id: regionId, sensors: regionSensors, elements: regionElements);
+                        Global.LocalStorage.SaveRegion(region);
+                        break;
+                    case "#Switch":
+                        int switchId = int.Parse(((UriNode)b.Subject).Uri.Fragment.Substring(2));
+                        Position switchCurrentPosition = Position.FAILURE;
+                        foreach (var triple in g.GetTriplesWithSubject(b.Subject))
+                        {
+                            switch (triple.Predicate.ToString().Split('#')[1])
+                            {
+                                case "currentPosition":
+                                    // az Id típusú mezőknél 2 karaktert kell levágni, mert "#_" kezdődik, itt viszont csak a '#' lesz benne
+                                    switchCurrentPosition = getPositionFromString(((UriNode)triple.Object).Uri.Fragment.Substring(1));
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        Switch sw = new Switch(cell_id: switchId, currentPosition: switchCurrentPosition);
+                        Global.LocalStorage.SaveSwitch(sw);
+                        break;
+                    case "#Sensor":
+                        int sensorId = int.Parse(((UriNode)b.Subject).Uri.Fragment.Substring(2));
+                        Sensor sensor = new Sensor(cell_id: sensorId);
+                        Global.LocalStorage.SaveSensor(sensor);
+                        break;
+                    case "#Segment":
+                        int segmentId = int.Parse(((UriNode)b.Subject).Uri.Fragment.Substring(2));
+                        int segmentLength = -1;
+                        List<int> segmentSemaphores = new List<int>();
+                        foreach (var triple in g.GetTriplesWithSubject(b.Subject))
+                        {
+                            switch (triple.Predicate.ToString().Split('#')[1])
+                            {
+                                case "length":
+                                    segmentLength = int.Parse(((LiteralNode)triple.Object).Value);
+                                    break;
+                                case "semaphores":
+                                    segmentSemaphores.Add(int.Parse(((UriNode)triple.Object).Uri.Fragment.Substring(2)));
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        Segment segment = new Segment(cell_id: segmentId, length: segmentLength, semaphores: segmentSemaphores);
+                        Global.LocalStorage.SaveSegment(segment);
+                        break;
+                    case "#SwitchPosition":
+                        int switchPositionId = int.Parse(((UriNode)b.Subject).Uri.Fragment.Substring(2));
+                        Position switchPositionPosition = Position.FAILURE;
+                        int switchPositionRoute = -1;
+                        int switchPositionTarget = -1;
+                        foreach (var triple in g.GetTriplesWithSubject(b.Subject))
+                        {
+                            switch (triple.Predicate.ToString().Split('#')[1])
+                            {
+                                case "position":
+                                    switchPositionPosition = getPositionFromString(((UriNode)triple.Object).Uri.Fragment.Substring(1));
+                                    break;
+                                case "route":
+                                    switchPositionRoute = int.Parse(((UriNode)triple.Object).Uri.Fragment.Substring(2));
+                                    break;
+                                case "target":
+                                    switchPositionTarget = int.Parse(((UriNode)triple.Object).Uri.Fragment.Substring(2));
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        SwitchPosition switchPosition = new SwitchPosition(cell_id: switchPositionId, position: switchPositionPosition, route: switchPositionRoute, target: switchPositionTarget);
+                        Global.LocalStorage.SaveSwitchPosition(switchPosition);
+                        break;
+                    case "#TrackElement":
+                        //egyelőre
+                        break;
+                    default:
+                        Console.WriteLine("Error: unknown type: " + ((UriNode)b.Object).Uri.Fragment);
+                        break;
+                }
+            }
+            Global.LocalStorage.SaveStorage();
+            //var semaphores = from sema in Global.LocalStorage.Semaphore_Accessor_Selector()
+            //                 select sema.signal;
+            //foreach (var semaphore in semaphores)
+            //{
+            //    Console.WriteLine(semaphore.ToString());
+            //}
+            //Console.ReadLine();
+        }
+
+        private static Signal getSignalFromString(string s)
+        {
+            switch (s)
+            {
+                case "SIGNAL_FAILURE":
+                    return Signal.FAILURE;
+                case "SIGNAL_STOP":
+                    return Signal.STOP;
+                case "SIGNAL_GO":
+                    return Signal.GO;
+                default:
+                    return Signal.FAILURE;
+            }
+        }
+
+        private static Position getPositionFromString(string s)
+        {
+            switch (s)
+            {
+                case "POSITION_FAILURE":
+                    return Position.FAILURE;
+                case "POSITION_STATE":
+                    return Position.STATE;
+                case "POSITION_DIVERGING":
+                    return Position.DIVERGING;
+                default:
+                    return Position.FAILURE;
+            }
         }
     }
 }
